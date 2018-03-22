03477917b7c0f7ab29938858f14cb437794bf1f5
Monika <mmonikawielgus@gmail.com>
2018-03-22 13:38:03 +0100

    Add all files

---
+++ CompetitionTracker/Controllers/ContestantsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CompetitionTracker.Models;
+
+namespace CompetitionTracker.Controllers
+{
+    public class ContestantsController : ApiController
+    {
+        Contestant[] contestants =
+        {
+            new Contestant { ContestantId = 1, FirstName = "Adam", LastName = "Nowak"},
+            new Contestant { ContestantId = 2, FirstName = "Jan", LastName = "Kowalski"},
+            new Contestant { ContestantId = 3, FirstName = "Anna", LastName = "Małecka"}
+        };
+        [HttpGet, ActionName("GetAllContestant")]
+        public HttpResponseMessage GetAllContestants()
+        {
+            Result result;
+            try
+            {
+                var contestantsList = contestants.ToList();
+                if(contestantsList.Lenght) 
+            }
+
+        }
+
+        public IHttpActionResult GetContestant(int id)
+        {
+            Contestant contestant = contestants.FirstOrDefault(c => c.ContestantId == id);
+            if (contestant == null)
+            {
+                return NotFound();
+            }
+            return Ok(contestant);
+        }
+    }
+}
+}
---
+++ CompetitionTracker/Models/Contestant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CompetitionTracker.Models
+{
+    public class Contestant: Result
+    {
+        public long ContestantId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        //public Contestant(string firstName, string lastName)
+        //{
+        //    FirstName = firstName;
+        //    LastName = lastName;
+        //}
+    }    
+}
