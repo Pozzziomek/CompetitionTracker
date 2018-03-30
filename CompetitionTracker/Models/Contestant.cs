bf229ac686fa9ee3ee1b8ee848a772df13c29432
Monika <mmonikawielgus@gmail.com>
2018-03-30 03:02:29 +0200

    Add contestant model

--- CompetitionTracker/Models/Contestant.cs
+++ CompetitionTracker/Models/Contestant.cs
@@ -5,18 +5,29 @@ using System.Web;
 
 namespace CompetitionTracker.Models
 {
-    public class Contestant: Result
+    public class Contestant
     {
-        public long ContestantId { get; set; }
+        private static long _nextId;
+
+        public long ContestantId { get; }
 
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
 
-        //public Contestant(string firstName, string lastName)
-        //{
-        //    FirstName = firstName;
-        //    LastName = lastName;
-        //}
+        public Contestant(string firstName, string lastName)
+        {
+            //if (String.IsNullOrEmpty(firstName))
+            //{
+            //    throw new ArgumentException("FirstName must be specified");
+            //}
+            //if (String.IsNullOrEmpty(lastName))
+            //{
+            //    throw new ArgumentException("LastName must be specified");
+            //}
+            ContestantId = ++_nextId;
+            FirstName = firstName;
+            LastName = lastName;
+        }
     }    
 }
