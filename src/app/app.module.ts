1ce8f38dfe0be5dd7b0d6bdb2b5ee2fcf6d858eb
Monika <mmonikawielgus@gmail.com>
2018-04-01 00:33:06 +0200

    Add route for contestants list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { ContestantsListComponent } from './contestants-views/contestants-list/contestants-list.component';
 
+import { HttpModule } from '@angular/http';
+import { RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContestantsListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+         path: 'contestants',
+         component: ContestantsListComponent
+      }
+   ])
   ],
   providers: [],
   bootstrap: [AppComponent]
