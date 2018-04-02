98db5ad0d584936e72c23c9c6495b6a1d72e5298
Monika <mmonikawielgus@gmail.com>
2018-04-02 03:19:33 +0200

    Add button which redirect to form for creating new contestant

--- src/app/contestants-views/contestants-list/contestants-list.component.ts
+++ src/app/contestants-views/contestants-list/contestants-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Contestant } from '../../model/contestant';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-contestants-list',
@@ -11,7 +12,8 @@ import { Contestant } from '../../model/contestant';
 export class ContestantsListComponent implements OnInit {
 
   contestants: Array<Contestant> = [];
-  constructor(private http: Http) { }
+  
+  constructor(private http: Http, private router: Router) { }
 
   ngOnInit() {
     this.http.get("http://localhost:57527/api/contestants")
@@ -21,7 +23,14 @@ export class ContestantsListComponent implements OnInit {
 
   displayData(data) {
     data.forEach(element => {
-      this.contestants.push(new Contestant(element.FirstName, element.LastName))
+      let o = new Contestant();
+      o.firstName = element.FirstName;
+      o.lastName = element.LastName;
+      this.contestants.push(o);
     });
+  }
+
+  goToContestantForm(event) {
+    this.router.navigate(['/contestant']);
   }
 }
