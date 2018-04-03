dd2f52a117b9b53032a739bc6b367a4b88e8ab4b
Monika <mmonikawielgus@gmail.com>
2018-04-04 00:19:42 +0200

    Add server validation to route model

--- CompetitionTrackerServer/CompetitionTracker/Models/Route.cs
+++ CompetitionTrackerServer/CompetitionTracker/Models/Route.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,23 @@ namespace CompetitionTracker.Models
 {
     public class Route
     {
-        public long RouteId { get; set; }
+        private static long _nextId;
 
+        public long RouteId { get; }
+
+        [Required(ErrorMessage = "Nazwa trasy jest wymagana.")]
+        [MaxLength(100, ErrorMessage = "Nazwa trasy nie może być dłuższa niż 100 znaków.")]
         public string RouteName { get; set; }
+
+        [Required(ErrorMessage = "Liczba punktów dla danej trasy jest wymagana.")]
+        [Range(10, 1000, ErrorMessage = "Liczba punktów powinna obejmować zakres 10-1000.")]
+        public int Points { get; set; }
+
+        public Route(string routeName, int points)
+        {
+            RouteId = ++_nextId;
+            RouteName = routeName;
+            Points = points;
+        }
     }
 }
