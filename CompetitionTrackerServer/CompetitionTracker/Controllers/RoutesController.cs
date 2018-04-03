2e622f2b33120198d0f9a131d4a9502867a4a796
Monika <mmonikawielgus@gmail.com>
2018-04-04 01:05:13 +0200

    Add route controller

---
+++ CompetitionTrackerServer/CompetitionTracker/Controllers/RoutesController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CompetitionTracker.Exceptions;
+using CompetitionTracker.Helpers;
+using CompetitionTracker.Models;
+
+namespace CompetitionTracker.Controllers
+{
+    public class RoutesController : ApiController
+    {
+        [HttpOptions]
+        public HttpResponseMessage Post(object json)
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        [HttpPost]
+        public HttpResponseMessage AddRoute([FromBody] Route route)
+        {
+            if (ModelState.IsValid)
+            {
+                RoutesSample.AddRoute(route.RouteName, route.Points);
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetAllRoutes()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, RoutesSample.GetAllRoutes());
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetRoute(long id)
+        {
+            try
+            {
+                Route route = RoutesSample.GetRoute(id);
+                return Request.CreateResponse(HttpStatusCode.OK, route);
+            }
+            catch (RouteNotFoundException e)
+            {
+                //TODO: logowanie wyjątków! 
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+        }
+
+        [HttpPut]
+        public HttpResponseMessage UpdateRouteInfo(long id, [FromBody] Route route)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Route updatedRoute = RoutesSample.UpdateRouteInfo(id, route.RouteName, route.Points);
+                    return Request.CreateResponse(HttpStatusCode.OK, updatedRoute);
+                }
+                catch (RouteNotFoundException e)
+                {
+                    return new HttpResponseMessage(HttpStatusCode.NotFound);
+                }
+            }
+            return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+        }
+
+        [HttpDelete]
+        public HttpResponseMessage DeleteRoute(int id)
+        {
+            try
+            {
+                RoutesSample.DeleteRoute(id);
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            catch (RouteNotFoundException e)
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+        }
+    }
+}
