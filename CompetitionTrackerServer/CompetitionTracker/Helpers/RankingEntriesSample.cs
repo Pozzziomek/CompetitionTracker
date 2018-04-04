d391c4ac59189b9d19b419a37c8268ce745170b0
Monika <mmonikawielgus@gmail.com>
2018-04-04 04:56:44 +0200

    [WIP] Add sample of ranking entries and methods to get, delete and add it

---
+++ CompetitionTrackerServer/CompetitionTracker/Helpers/RankingEntriesSample.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CompetitionTracker.Exceptions;
+using CompetitionTracker.Models;
+
+namespace CompetitionTracker.Helpers
+{
+    public class RankingEntriesSample
+    {
+        public static readonly List<RankingEntry> RankingEntries = new List<RankingEntry>()
+        {
+            
+        };
+
+        public static void AddRankingEntry(Contestant contestant, Route route)
+        {
+            RankingEntry rankingEntry = new RankingEntry(contestant, route);
+            RankingEntries.Add(rankingEntry);
+        }
+
+        public static List<RankingEntry> GetRankingEntries()
+        {
+            return RankingEntries;
+        }
+
+        public static void DeleteRankingEntry(long id)
+        {
+            RankingEntry rankingEntry = FindRankingEntry(id);
+            RankingEntries.Remove(rankingEntry);
+        }
+
+        private static RankingEntry FindRankingEntry(long id)
+        {
+            RankingEntry rankingEntry = RankingEntries.FirstOrDefault(r => r.RankingEntryId == id);
+            if (rankingEntry == null)
+            {
+                throw new RankingEntryNotFoundException($"Contestant with id {id} in ranking not found!");
+            }
+            return rankingEntry;
+        }
+    }
+}
